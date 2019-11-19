b6f72216d2c09f0588c683c950a532f921fdcc79
Rybin Andrey <rybin.ag@gmail.com>
2019-11-19 15:21:49 +0300

    linter correction

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 import command from 'commander';
-//const program = require('commander');
-const  program = command;
+
+const program = command;
 program
-.description('Compares two configuration files and shows a difference.')
-.option('-V, --version', 'output the version number', '1.0.0')
-.option('-f, --format [type]', 'output format' )
-.arguments('<firstConfig> <secondConfig>');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-V, --version', 'output the version number', '1.0.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>');
 
 
 program.parse(process.argv);
-
-  

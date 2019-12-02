0012a2fb0375774fa40df716ddd5e1cca6259bf1
Rybin Andrey <rybin.ag@gmail.com>
2019-12-02 16:05:26 +0300

    clearing levels for idents

--- src/index.js
+++ src/index.js
@@ -1,56 +1,40 @@
 import _ from 'lodash';
 import parser from './parser';
 
-
-const objToConfigStr = (obj) => {
-  const keys = Object.keys(obj);
-  return keys.reduce((acc, key) => `${acc}\n+ ${key}: ${obj[key]}`, '');
-};
-
-
-const stringify = (obj, level, accStr) => {
+const stringify = (obj, indentLevel, accStr) => {
   const keys = Object.keys(obj);
   return keys.reduce((keyAcc, key) => {
-    const newlevel = level + 1;
-    const value = obj[key] instanceof Object ? stringify(obj[key], newlevel, accStr) : obj[key];
-    return `${keyAcc}\n${' '.repeat(4 * level)}${key}: ${value}`;
+    const newIndentLevel = indentLevel + 1;
+    const value = obj[key] instanceof Object ? stringify(obj[key], newIndentLevel, accStr)
+      : `${obj[key]}    level = ${newIndentLevel}`;
+    return `${keyAcc}\n${key}: ${value}`;
   }, accStr);
 };
 
-
-const astRender = (diffTree, level, strAcc) => {
-  return diffTree.reduce((accKey, curObj) => {
-    if (curObj.state === 'deleted') {
-      const newlevel = level + 1;
-      const show = curObj.value instanceof Object ? stringify(curObj.value, newlevel, '') : curObj.value;
-      return `${accKey}\n - ${curObj.key}: ${show}`;
-    }
-    if (curObj.state === 'unchanged') {
-      const newlevel = level + 1;
-      const show = curObj.value instanceof Object ? stringify(curObj.value, newlevel, '') : curObj.value;
-      return `${accKey}\n   ${curObj.key}: ${show}`;
-    }
-    if (curObj.state === 'added') {
-      const newlevel = level + 1;
-      const show = curObj.value instanceof Object ? stringify(curObj.value, newlevel, '') : curObj.value;
-      return `${accKey}\n + ${curObj.key}: ${show}`;
-    }
-    if (curObj.children.length < 1) {
-      const newlevel = level + 1;
-      const obj1 = curObj.value[0];
-      const obj2 = curObj.value[1];
-      const show1 = obj1 instanceof Object ? stringify(obj1, newlevel, '') : obj1;
-      const show2 = obj2 instanceof Object ? stringify(obj2, newlevel, '') : obj2;
-      return `${accKey}\n - ${curObj.key}: ${show1}\n + ${curObj.key}: ${show2}`;
-    }
-    const obj1 = curObj.children[0];
-    const obj2 = curObj.children[1];
-    const show1 = obj1 instanceof Object ? stringify(obj1, newlevel, '') : obj1;
-    const show2 = obj2 instanceof Object ? stringify(obj2, newlevel, '') : obj2;
-
-  },
-  strAcc);
+const states = {
+  deleted: '-',
+  unchanged: ' ',
+  added: '+',
 };
+
+const getItemForPrint = (obj, indent) => (obj instanceof Object ? stringify(obj, indent, '') : `${obj}  level=${indent}`);
+
+
+const astRender = (diffTree, level, strAcc) => diffTree.reduce((accKey, curObj) => {
+  if (_.has(states, curObj.state)) {
+    return `${accKey}\n${states[curObj.state]} ${curObj.key}: ${getItemForPrint(curObj.value, level)}`;
+  }
+  if (curObj.children.length < 1) {
+    const obj1 = curObj.value[0];
+    const obj2 = curObj.value[1];
+    return `${accKey}\n - ${curObj.key}: ${getItemForPrint(obj1, level)}\n+ ${curObj.key}: ${getItemForPrint(obj2, level)}`;
+  }
+  const arg = curObj.children;
+  const levelup = level +1;
+  return `${accKey}\n  ${curObj.key}:   level=${level} ${astRender(arg, levelup, strAcc)}`;
+},
+strAcc);
+
 
 const diff = (firstFilePath, secondFilePath) => {
   const objBefore = parser(firstFilePath);
@@ -92,8 +76,6 @@ const diff = (firstFilePath, secondFilePath) => {
   };
 
   const ast = makeDiffAST(objBefore, objAfter, []);
-  //console.log(astRender(ast));
-  const objstr = ` + ${stringify(objBefore, 0, '')}`;
   console.log(astRender(ast, 0, ''));
   return astRender(ast, 0, '');
 };
